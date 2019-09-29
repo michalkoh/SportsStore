f759fe446ae63ac3b96011d2f347cde15d90f170
michalkoh <michalkoh@gmail.com>
2019-09-29 14:50:11 +0200

    Basic style added.

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -24,6 +24,14 @@ namespace SportsStore.Infrastructure
 
         public string PageAction { get; set; }
 
+        public bool PageClassEnabled { get; set; } = false;
+
+        public string PageClass { get; set; }
+
+        public string PageClassNormal { get; set; }
+
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var urlHelper = this.urlHelperFactory.GetUrlHelper(ViewContext);
@@ -32,6 +40,11 @@ namespace SportsStore.Infrastructure
             {
                 var tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(new UrlActionContext() { Action = PageAction, Values = new { page = i } });
+                if (PageClassEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.AppendHtml(i.ToString() + " ");
                 result.InnerHtml.AppendHtml(tag);
             }
